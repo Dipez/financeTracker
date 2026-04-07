4aded2cee49661fd80c93d32749b5c89be2394d3
Dipez <aaryalimbu90@gmail.com>
2026-04-07 21:47:10 +0545

    feat: added ksp plugin and dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -43,6 +44,7 @@ android {
 }
 
 dependencies {
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
 
@@ -55,9 +57,22 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui.text)
 
-    // 🔥 ADD THIS (VERY IMPORTANT)
+    // ✅ ViewModel + Navigation
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    // 🔥🔥 ADD THESE (YOU MISSED THESE)
+    implementation("androidx.room:room-runtime:2.6.1")
+
+    // ✅ IMPORTANT (KSP instead of kapt)
+    ksp("androidx.room:room-compiler:2.6.1")
+
+    implementation("androidx.room:room-ktx:2.6.1")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    // Tests
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
